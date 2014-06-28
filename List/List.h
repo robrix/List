b36f387e48782cf1ae0e476d2327e01bc50fccb0
Rob Rix <rob.rix@github.com>
2014-06-28 11:44:06 -0400

    Tidy up the header & tests.

--- List/List.h
+++ List/List.h
@@ -1,19 +1,7 @@
-//
-//  List.h
-//  List
-//
-//  Created by Rob Rix on 2014-06-28.
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
-//
-
-#import <Cocoa/Cocoa.h>
 
 //! Project version number for List.
-FOUNDATION_EXPORT double ListVersionNumber;
+extern double ListVersionNumber;
 
 //! Project version string for List.
-FOUNDATION_EXPORT const unsigned char ListVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <List/PublicHeader.h>
-
-
+extern const unsigned char ListVersionString[];
