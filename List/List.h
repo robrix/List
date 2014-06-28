81e702f6605606d06de2eef2086a605c6e0c9b34
Rob Rix <rob.rix@github.com>
2014-06-28 11:40:55 -0400

    Initial Commit

---
+++ List/List.h
@@ -0,0 +1,19 @@
+//
+//  List.h
+//  List
+//
+//  Created by Rob Rix on 2014-06-28.
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for List.
+FOUNDATION_EXPORT double ListVersionNumber;
+
+//! Project version string for List.
+FOUNDATION_EXPORT const unsigned char ListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <List/PublicHeader.h>
+
+
